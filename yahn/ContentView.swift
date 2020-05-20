cf35e56821bb940f57d79759fba02fd25ae4c678
Faiz Mokhtar <mfaizmokhtar@airasia.com>
2020-05-20 23:03:48 +0800

    Initial Commit

---
+++ yahn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yahn
+//
+//  Created by Faiz Mokhtar on 20/05/2020.
+//  Copyright © 2020 Faiz Mokhtar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
